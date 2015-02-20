61bd6e8026d4f9cbda27553e0ddfbcae1b6540d0
Joao Teodoro <joaotdn@gmail.com>
2015-02-19 23:23:27 -0300

    Carro atualizada

--- MLP_SPI__2015_1_LISTA1/src/Programa.java
+++ MLP_SPI__2015_1_LISTA1/src/Programa.java
@@ -1,10 +1,12 @@
+import fatura.Fatura;
+
 
 public class Programa {
 	public static void main(String[] args) {
 		Fatura fatura = new Fatura();
 		
 		fatura.setPreco(10);
-		fatura.setQuantidade(9);
+		fatura.setQuantidade(-9);
 		
 		System.out.println(fatura.getValorFatura());
 	}
--- MLP_SPI__2015_1_LISTA1/src/automovel/Carro.java
+++ MLP_SPI__2015_1_LISTA1/src/automovel/Carro.java
@@ -1,5 +1,47 @@
 package automovel;
 
 public class Carro {
+	private String cor;
+	private String modelo;
+	private int velocidadeAtual;
+	private int velocidadeMaxima;
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getVelocidadeAtual() {
+		return velocidadeAtual;
+	}
+
+	public void setVelocidadeAtual(int velocidadeAtual) {
+		this.velocidadeAtual = velocidadeAtual;
+	}
+
+	public int getVelocidadeMaxima() {
+		return velocidadeMaxima;
+	}
+
+	public void setVelocidadeMaxima(int velocidadeMaxima) {
+		this.velocidadeMaxima = velocidadeMaxima;
+	}
+
+	public void acelerar() {
+		if (this.velocidadeAtual > this.velocidadeMaxima) {
+			System.out.println("A velocidade foi excedida");
+		}
+	}
 
 }
